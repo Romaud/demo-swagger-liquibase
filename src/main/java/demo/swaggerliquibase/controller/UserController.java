47d635c4a40248b0a683655cc8bd2495abc213ce
Romaud <ramoon.doc@gmail.com>
2022-11-19 23:41:15 +0300

    добавление и настройка swagger

--- src/main/java/demo/swaggerliquibase/controller/UserController.java
+++ src/main/java/demo/swaggerliquibase/controller/UserController.java
@@ -2,14 +2,20 @@ package demo.swaggerliquibase.controller;
 
 import demo.swaggerliquibase.entity.User;
 import demo.swaggerliquibase.service.UserServiceImpl;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@Tag(name = "User controller")
 @RequestMapping("/api")
 @RestController
 @AllArgsConstructor
-public class MyController {
+public class UserController {
     UserServiceImpl userService;
 
     @GetMapping("/hello")
@@ -18,9 +24,16 @@ public class MyController {
     }
 
     @PostMapping
+    @Operation(description = "User create")
     public ResponseEntity<User> createUser(@RequestBody User user) {
         System.out.println(user);
         userService.save(user);
         return ResponseEntity.ok(user);
+    }
+
+    @Operation(description = "Get all user")
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
     }
 }
--- src/main/java/demo/swaggerliquibase/service/UserServiceImpl.java
+++ src/main/java/demo/swaggerliquibase/service/UserServiceImpl.java
@@ -5,17 +5,19 @@ import demo.swaggerliquibase.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @AllArgsConstructor
 public class UserServiceImpl {
     UserRepository userRepository;
 
     public User save(User user) {
-        System.out.println("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
-        System.out.println(user);
-        User usr = userRepository.save(user);
-        System.out.println("------------------------------");
-        System.out.println(usr);
-        return usr;
+        return userRepository.save(user);
     }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
 }
